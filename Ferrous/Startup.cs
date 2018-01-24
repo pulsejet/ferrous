1ae751376a3b06f6fba65b17f2f17fc51469d0bf
RadialApps <radialapps@gmail.com>
2018-01-24 13:36:58 +0530

    Add publish to plain windows, Enable webpack middleware only when debugger attached

--- Ferrous/Startup.cs
+++ Ferrous/Startup.cs
@@ -40,7 +40,7 @@ namespace Ferrous
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() && System.Diagnostics.Debugger.IsAttached)
             {
                 app.UseDeveloperExceptionPage();
                 app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
