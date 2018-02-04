a7ccb6bcb075eba674f4b6c0fdb84c2f11ad4212
RadialApps <radialapps@gmail.com>
2018-02-04 18:38:59 +0530

    Create DataUtilites

--- Ferrous/Controllers/ContingentsController.cs
+++ Ferrous/Controllers/ContingentsController.cs
@@ -30,38 +30,8 @@ namespace Ferrous.Controllers
                 Response.StatusCode = StatusCodes.Status401Unauthorized;
                 return null;
             }
-
-            Contingents[] cts = await _context.Contingents
-                                        .Include(m => m.Person)
-                                        .Include(m => m.ContingentArrival).ToArrayAsync();
-
-            foreach (var contingent in cts)
-            {
-                int RegMale = 0, RegFemale = 0, 
-                    ArrivedM = 0, ArrivedF = 0, 
-                    OnSpotM = 0, OnSpotF = 0;
-
-                Parallel.ForEach(contingent.Person, person => {
-                    if (person == null) return;
-                    if (person.Sex == "M") RegMale += 1;
-                    else if (person.Sex == "F") RegFemale += 1;
-                });
-
-                Parallel.ForEach(contingent.ContingentArrival, ca =>
-                {
-                    ArrivedM += dbCInt(ca.Male);
-                    ArrivedF += dbCInt(ca.Female);
-                    OnSpotM += dbCInt(ca.MaleOnSpot);
-                    OnSpotF += dbCInt(ca.FemaleOnSpot);
-                });
-
-                contingent.Male = RegMale.ToString();
-                contingent.Female = RegFemale.ToString();
-                contingent.ArrivedM = ArrivedM.ToString() + ((OnSpotM > 0) ? " + " + OnSpotM : "");
-                contingent.ArrivedF = ArrivedF.ToString() + ((OnSpotF > 0) ? " + " + OnSpotF : "");
-            }
-
-            return _context.Contingents;
+            
+            return await DataUtilities.GetExtendedContingents(_context);
         }
 
         // GET: api/Contingents/5
---
+++ Ferrous/Controllers/DataUtilities.cs
@@ -0,0 +1,48 @@
+﻿using Ferrous.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Ferrous.Utilities;
+
+namespace Ferrous.Controllers
+{
+    public class DataUtilities
+    {
+        public async static Task<Contingents[]> GetExtendedContingents(ferrousContext _context)
+        {
+            Contingents[] cts = await _context.Contingents
+                                        .Include(m => m.Person)
+                                        .Include(m => m.ContingentArrival).ToArrayAsync();
+
+            foreach (var contingent in cts)
+            {
+                int RegMale = 0, RegFemale = 0,
+                    ArrivedM = 0, ArrivedF = 0,
+                    OnSpotM = 0, OnSpotF = 0;
+
+                Parallel.ForEach(contingent.Person, person => {
+                    if (person == null) return;
+                    if (person.Sex == "M") RegMale += 1;
+                    else if (person.Sex == "F") RegFemale += 1;
+                });
+
+                Parallel.ForEach(contingent.ContingentArrival, ca =>
+                {
+                    ArrivedM += dbCInt(ca.Male);
+                    ArrivedF += dbCInt(ca.Female);
+                    OnSpotM += dbCInt(ca.MaleOnSpot);
+                    OnSpotF += dbCInt(ca.FemaleOnSpot);
+                });
+
+                contingent.Male = RegMale.ToString();
+                contingent.Female = RegFemale.ToString();
+                contingent.ArrivedM = ArrivedM.ToString() + ((OnSpotM > 0) ? " + " + OnSpotM : "");
+                contingent.ArrivedF = ArrivedF.ToString() + ((OnSpotF > 0) ? " + " + OnSpotF : "");
+            }
+
+            return cts;
+        }
+    }
+}
