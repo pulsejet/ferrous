b584bd70042bc1b2e8087903541414cb083ad6ec
RadialApps <radialapps@gmail.com>
2018-02-20 12:52:40 +0530

    Comment LinkHelper

--- Ferrous/Misc/Links.cs
+++ Ferrous/Misc/Links.cs
@@ -42,6 +42,13 @@ namespace Ferrous.Misc
         private List<Link> Links = new List<Link>();
         private IUrlHelper urlHelper;
 
+        /// <summary>
+        /// Set options for the LinkHelper
+        /// </summary>
+        /// <param name="user">User object for authorization</param>
+        /// <param name="type">Type of the container of the method</param>
+        /// <param name="urlHelper">Url object for Url generation</param>
+        /// <returns></returns>
         public LinkHelper SetOptions(ClaimsPrincipal user, Type type, IUrlHelper urlHelper)
         {
             this.user = user;
@@ -52,9 +59,11 @@ namespace Ferrous.Misc
 
         public LinkHelper AddLink(string route, object routeParams = null, string overrideWithRel = "")
         {
+            /* Get the method */
             MethodInfo controllerMethod = type.GetMethod(route);
             Authorization attr = (Authorization)controllerMethod.GetCustomAttributes(typeof(Authorization), true)[0];
 
+            /* Get the relevant attributes */
             var getAtt = (HttpGetAttribute) controllerMethod.GetCustomAttribute(typeof(HttpGetAttribute));
             var postAtt = (HttpPostAttribute) controllerMethod.GetCustomAttribute(typeof(HttpPostAttribute));
             var putAtt = (HttpPutAttribute) controllerMethod.GetCustomAttribute(typeof(HttpPutAttribute));
@@ -63,6 +72,7 @@ namespace Ferrous.Misc
             
             string httpMethod = HTTPMethod.GET;
 
+            /** Gets the routeing attribute that is being used */
             object routingAttribute = null;
 
             if (getAtt != null) { httpMethod = HTTPMethod.GET; routingAttribute = getAtt; }
@@ -70,12 +80,17 @@ namespace Ferrous.Misc
             else if (putAtt != null) { httpMethod = HTTPMethod.PUT; routingAttribute = putAtt; }
             else if (deleteAtt != null) { httpMethod = HTTPMethod.DELETE; routingAttribute = deleteAtt; }
 
+            /* Gets the route template for the controller method 
+             * that is being used to route the call
+             * TODO: One method may have multiple. Throw an error for this */
             string routeTemplate = "";
             if (routingAttribute != null)
                 routeTemplate = (string) routingAttribute.GetType().GetProperty(nameof(getAtt.Template)).GetValue(routingAttribute);
 
+            /* Throw an error if HTTPrel is not set */
             if (relAtt == null) throw new Exception("HTTPrel attribute not set for creating link");
 
+            /* Check for priveleges and add the link*/
             if (hasPrivilege(user.Identity.Name, attr._elevationLevel, attr._privilege))
             {
                 if (routeParams == null)
