a84d03ecadb697540a43b440297456ffef91ff4d
PulseJet <radialapps@gmail.com>
2018-02-28 22:44:49 +0530

    Merge app.browser.module and app.shared.module

--- Ferrous/ClientApp/app/app.browser.module.ts
+++ Ferrous/ClientApp/app/app.browser.module.ts
@@ -1,21 +1,97 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppModuleShared } from './app.shared.module';
 import { AppComponent } from './components/app/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { ContingentComponent } from './components/contingent/contingent.component';
+import { ContingentDetailsComponent } from './components/contingent-details/contingent-details.component';
+import { ContingentArrivalDialogComponent } from './components/contingent-arrival-dialog/contingent-arrival-dialog.component'
+
+import { PersonComponent } from './components/person/person.component';
+import { PersonDetailsComponent } from './components/person-details/person-details.component';
+
+import { RoomLayoutComponent } from './components/room-layout/room-layout.component';
+import { LocationSelectComponent } from './components/location-select/location-select.component';
+import { RoomDialogComponent } from './components/room-dialog/room-dialog.component'
+
+import { CounterComponent } from './components/counter/counter.component';
+import { MyMaterialClass } from './material-angular.module';
+import { DataService } from './data.service';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FilterContingents } from './pipes/filter-contingents';
+import { ClickStopPropagation } from './helpers';
+
 import 'hammerjs';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
+    bootstrap: [
+        AppComponent
+    ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+
+        ContingentComponent,
+        ContingentDetailsComponent,
+        ContingentArrivalDialogComponent,
+
+        PersonComponent,
+        PersonDetailsComponent,
+
+        RoomLayoutComponent,
+        LocationSelectComponent,
+        RoomDialogComponent,
+
+        HomeComponent,
+
+        ClickStopPropagation,
+        FilterContingents
+    ],
     imports: [
         BrowserModule,
-        AppModuleShared,
         BrowserAnimationsModule,
         //ServiceWorkerModule.register('/ngsw-worker.js')
+
+        CommonModule,
+        HttpClientModule,
+        FormsModule,
+        MyMaterialClass,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+
+            { path: 'contingent', component: ContingentComponent },
+            { path: 'contingentDetails/:id', component: ContingentDetailsComponent },
+            { path: 'contingentDetails/:id/:edit', component: ContingentDetailsComponent },
+
+            { path: 'person', component: PersonComponent },
+            { path: 'personDetails/:link', component: PersonDetailsComponent },
+            { path: 'personDetails/:link/:edit', component: PersonDetailsComponent },
+
+            { path: 'roomLayout/:link/:location/:clno', component: RoomLayoutComponent },
+            { path: 'locationSelect/:clno/:cano', component: LocationSelectComponent },
+
+            { path: '**', redirectTo: 'home' }
+        ])
     ],
     providers: [
-        { provide: 'BASE_URL', useFactory: getBaseUrl }
+        { provide: 'BASE_URL', useFactory: getBaseUrl },
+        DataService
+    ],
+    entryComponents: [
+        ContingentArrivalDialogComponent,
+        RoomDialogComponent
     ]
 })
 export class AppModule {
