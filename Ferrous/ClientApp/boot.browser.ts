d06deb55ecd3363514786a450a5a63705da56974
PulseJet <radialapps@gmail.com>
2018-02-28 22:47:17 +0530

    Remove old code from boot.browser

--- Ferrous/ClientApp/boot.browser.ts
+++ Ferrous/ClientApp/boot.browser.ts
@@ -7,14 +7,6 @@ import { AppModule } from './app/app.module';
 
 if (module.hot) {
     module.hot.accept();
-    module.hot.dispose(() => {
-        // TODO: Why are these commented for fixing HMR?
-        // Before restarting the app, we create a new root element and dispose the old one
-        // const oldRootElem = document.querySelector('app');
-        // const newRootElem = document.createElement('app');
-        // oldRootElem!.parentNode!.insertBefore(newRootElem, oldRootElem);
-        // modulePromise.then(appModule => appModule.destroy());
-    });
 } else {
     enableProdMode();
 }
