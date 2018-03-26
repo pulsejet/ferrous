2f93c9cf2a507aaf1f11be036abd5e8ed65de8ba
Varun Patil <radialapps@gmail.com>
2018-03-26 13:37:49 +0530

    Fix redundancy

--- Misc/LinkMakers.cs
+++ Misc/LinkMakers.cs
@@ -160,7 +160,7 @@ namespace Ferrous.Misc
 
             var linkHelper = new LinkHelper()
                 .SetOptions(User, typeof(RoomsController), Url)
-                .AddLink(nameof(RoomsController.GetRoom), new { id = room.RoomId, clno = clno, cano = cano })
+                .AddLink(nameof(RoomsController.GetRoom), new { id = room.RoomId, clno, cano })
                 .AddLink(nameof(RoomsController.PutRoom), idObject)
                 .AddLink(nameof(RoomsController.DeleteRoom), idObject);
 
